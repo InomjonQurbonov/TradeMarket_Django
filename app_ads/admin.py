7a9d210a92628ff4a76c1119502b8589a433c415
InomjonQurbonov <inomjonqurbonov916@gmail.com>
2025-05-30 22:24:12 +0500

    Admin is ready

--- app_ads/admin.py
+++ app_ads/admin.py
@@ -1,13 +1,21 @@
 from django.contrib import admin
-from models import AdsCategory, Advertiser
+from app_ads.models import AdsCategory, Advertiser
 
 class AdsCategoryAdmin(admin.ModelAdmin):
-    pass
+    list_filter = ('id', 'category_name')
+    list_display = ('id', 'category_name')
+    search_fields = ('category_name', 'category_info')
 
 
 class AdsAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('id','user_Id', 'ad_title','created_at', 'is_active')
+    list_filter = ('user_Id', 'ad_title', 'ad_category','ad_condition', 'is_active')
+    search_fields = (
+        'id', 'user_Id', 'ad_title', 'ad_description',
+        'ad_condition', 'ad_category', 'created_at',
+        'updated_at', 'is_active'
+                     )
 
 
-admin.site.register(AdsCategory)
-admin.site.register(Advertiser)
+admin.site.register(AdsCategory, AdsCategoryAdmin)
+admin.site.register(Advertiser, AdsAdmin)
